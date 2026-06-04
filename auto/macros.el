087694f0d8b447436824e273e5e52b7d7052339c
oscarharr <oscar.at.ofs@gmail.com>
2026-06-04 11:40:21 +0200

    starts rewriting + finishing Sec 1

---
+++ auto/macros.el
@@ -0,0 +1,160 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "macros"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "--shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("pict2e" "")))
+   (TeX-run-style-hooks
+    "pict2e")
+   (TeX-add-symbols
+    '("cptSections" "Text" ["Text"])
+    '("Sections" "Text" ["Text"])
+    '("mdef" 1)
+    '("tdef" 1)
+    '("ani" 1)
+    '("ind" 2)
+    '("ch" 2)
+    '("sSpan" 1)
+    '("Span" 1)
+    '("tr" 2)
+    '("integral" 2)
+    '("pPrl" 1)
+    '("LocSys" 1)
+    '("fctD" 1)
+    '("D" 1)
+    '("Onote" 1)
+    '("Fnote" 1)
+    "tset"
+    "Top"
+    "Sheaves"
+    "coShat"
+    "Prdualf"
+    "Prlstf"
+    "Prdual"
+    "ass"
+    "loc"
+    "dual"
+    "evalu"
+    "Trc"
+    "TrcL"
+    "Endtrl"
+    "bmH"
+    "bcat"
+    "freeondual"
+    "fundgroupoid"
+    "lcH"
+    "VD"
+    "Dl"
+    "Hom"
+    "Homd"
+    "can"
+    "all"
+    "map"
+    "Map"
+    "Spec"
+    "im"
+    "fib"
+    "cofib"
+    "id"
+    "Aut"
+    "Nat"
+    "End"
+    "et"
+    "Sp"
+    "Spc"
+    "Set"
+    "Prl"
+    "Prlst"
+    "Prr"
+    "Prrst"
+    "Mod"
+    "Vect"
+    "grVect"
+    "coMod"
+    "Cat"
+    "CAlg"
+    "QCoh"
+    "Ind"
+    "Pro"
+    "Ql"
+    "Qp"
+    "Gal"
+    "ev"
+    "coev"
+    "Fun"
+    "hShv"
+    "Shv"
+    "HH"
+    "THH"
+    "colim"
+    "Yo"
+    "BB"
+    "DD"
+    "EE"
+    "E"
+    "CC"
+    "RR"
+    "FF"
+    "F"
+    "bF"
+    "GG"
+    "QQ"
+    "ZZ"
+    "NN"
+    "cA"
+    "cB"
+    "cC"
+    "cD"
+    "cE"
+    "cF"
+    "cG"
+    "cH"
+    "cI"
+    "cJ"
+    "cK"
+    "cL"
+    "cM"
+    "cN"
+    "cO"
+    "cP"
+    "cQ"
+    "cR"
+    "cS"
+    "cT"
+    "cU"
+    "cV"
+    "cW"
+    "cX"
+    "cY"
+    "cZ"
+    "sF"
+    "sE"
+    "fr"
+    "cl"
+    "scr"
+    "bb"
+    "too"
+    "rar"
+    "lar"
+    "rari"
+    "empt"
+    "p"
+    "ul"
+    "sub"
+    "eps"
+    "blank"
+    "iso"
+    "pt"
+    "op"
+    "ab"
+    "hyp"
+    "noloc"
+    "fcolim"
+    "flim"
+    "cwedge"
+    "cvee"))
+ :latex)
+
---
+++ auto/sources.el
@@ -0,0 +1,25 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "sources"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "--shell-escape")
+   (LaTeX-add-bibitems
+    "Liu_Zheng2012"
+    "Scholze2025"
+    "Mann2022"
+    "Aoki2024"
+    "Cnossen_Lenz_Linskens2026"
+    "Beraldo_Pippi2024"
+    "Harr2026"
+    "Hoyois_Scherotzke_Sibilla2017"
+    "Lurie2017"
+    "MannHey2024"
+    "Volpe2023"
+    "Efimov2025"
+    "Ramzi2024"
+    "Krause_Nikolaus_Putzstuck2024"
+    "Nikolaus2024"))
+ '(or :bibtex :latex))
+
